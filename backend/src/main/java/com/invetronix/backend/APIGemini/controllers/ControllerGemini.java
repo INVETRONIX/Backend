b1f8e9bb551ed41fcd3874fb99114dc585df6783
Juan <pablobedoya3521@gmail.com>
2025-04-20 13:41:58 -0500

    Documentada api de imagenes

--- backend/src/main/java/com/invetronix/backend/APIGemini/controllers/ControllerGemini.java
+++ backend/src/main/java/com/invetronix/backend/APIGemini/controllers/ControllerGemini.java
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
